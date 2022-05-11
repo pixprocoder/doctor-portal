0cf758a6d468b848f0bd469b5980f34f544c5cb3
kabir <makkobir79@gmail.com>
2022-05-11 14:23:51 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-2xl text-yellow-200">Welcome to Milestone 12</h1>
+      <button class="btn btn-primary">Button</button>
+    </div>
+  );
+}
+
+export default App;
