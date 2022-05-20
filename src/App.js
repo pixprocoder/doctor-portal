0638059d398ee30000f8a94b7ab58f7a8d307b99
kabir <makkobir79@gmail.com>
2022-05-20 23:09:52 +0600

    footer and router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./Pages/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Routes>{/* <Route path="/" element={<Home/>}/> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </>
   );
 };
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="bg-slate-600">This is footer</div>;
+};
+
+export default Footer;
