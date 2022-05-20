8b9a385480e59bfc7a27b6ad53042d6d2d8ebc4e
kabir <makkobir79@gmail.com>
2022-05-20 22:48:32 +0600

    navbar addded

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import About from "../src/Pages/About/About";
-import Login from "./Pages/Login/Login";
-import NotFound from "./Pages/Shared/404Page/NotFound";
-import Header from "./Pages/Shared/Header/Header";
-import Review from "./Pages/Home/Review";
-import Contact from "./Pages/Home/Contact";
-import Appointment from "./Pages/Appointment/Appointment";
-import Signup from "./Pages/Login/Signup";
+import Navbar from "./Pages/Navbar/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="max-w-7xl mx-auto">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/appointment" element={<Appointment />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/review" element={<Review />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <>
+      <Navbar />
+      <Routes>{/* <Route path="/" element={<Home/>}/> */}</Routes>
+    </>
   );
-}
+};
 
 export default App;
