b561a874d05c5501d3af2e0236e3e7f2682a6ac3
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2023-01-14 02:33:01 +0600

    banner half completed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,22 +1,21 @@
 import React from "react";
+import chair from "../../../assets/images/chair.png";
 
 export default function Banner() {
   return (
-    <div className="hero bg-base-200">
+    <div className="hero">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="https://placeimg.com/260/400/arch"
-          className="max-w-sm rounded-lg shadow-2xl"
-          alt=""
-        />
+        <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" />
         <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <h1 className="text-5xl font-bold">Welcome to our world!</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">
+            Explore now
+          </button>
         </div>
       </div>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -25,7 +25,7 @@ export default function Header() {
     </>
   );
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar flex justify-between">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
