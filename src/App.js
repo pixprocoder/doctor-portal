59a841d013ecec3252b5745d5e8b61cfb29b7c4a
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2023-01-13 18:20:20 +0600

    css added daisyui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <h1 className="text-red-500 font-bold text-center">hello</h1>
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,18 @@ module.exports = {
   theme: {
     extend: {},
   },
-  // daisyui: {
-  //   themes: [
-  //     {
-  //       DoctorPortalTheme: {
-  //         primary: "#0FCFEC",
+  daisyui: {
+    themes: [
+      {
+        DoctorPortalTheme: {
+          primary: "#0FCFEC",
 
-  //         secondary: "#19D3AE",
+          secondary: "#19D3AE",
 
-  //         accent: "#3A4256",
-  //       },
-  //     },
-  //   ],
-  // },
+          accent: "#3A4256",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
