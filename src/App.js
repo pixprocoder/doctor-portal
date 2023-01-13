1dc648f10bfc5d6f189e61e50fec483b89c99aca
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2023-01-13 18:08:12 +0600

    re building the project setup

--- src/App.js
+++ src/App.js
@@ -1,58 +1,9 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import About from "./Pages/About/About";
-import Appointment from "./Pages/Appointment/Appointment";
-import ContactUs from "./Pages/Contact/ContactUs";
-import Home from "./Pages/Home/Home";
-import Login from "./Pages/Login/Login";
-import RequireAuth from "./Pages/Login/RequireAuth";
-import Signup from "./Pages/Login/Signup";
-import Reviews from "./Pages/Reviews/Reviews";
-import Navbar from "./Pages/Shared/Navbar/Navbar";
-import NotFound from "./Pages/Shared/NotFound";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import MyAppointment from "./Pages/Dashboard/MyAppointment";
-import MyReview from "./Pages/Dashboard/MyReview";
 
 const App = () => {
   return (
-    <div className="max-w-screen-xl mx-auto">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/review" element={<Reviews />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route
-          path="/appointment"
-          element={
-            <RequireAuth>
-              <Appointment />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <RequireAuth>
-              <Dashboard />
-            </RequireAuth>
-          }
-        >
-          <Route index element={<MyAppointment></MyAppointment>}></Route>
-          <Route
-            path="/dashboard/review"
-            element={<MyReview></MyReview>}
-          ></Route>
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <ToastContainer></ToastContainer>
+    <div>
+      <h1>hello</h1>
     </div>
   );
 };
