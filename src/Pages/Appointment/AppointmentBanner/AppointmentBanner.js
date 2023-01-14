db8b078b28aa9c459cc018f159ec690e3175780e
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2023-01-15 02:49:40 +0600

    react-day-picker and date-fns added

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,22 +1,29 @@
 import React from "react";
+import { DayPicker } from "react-day-picker";
 import chair from "../../../assets/images/chair.png";
 import bg from "../../../assets/images/bg.png";
+import { useState } from "react";
+import { format } from "date-fns";
 
 export default function AppointmentBanner() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   return (
     <header
       style={{
         background: `url(${bg})`,
         backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
       }}
     >
       <div className="hero ">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" />
           <div>
-            <h1>calender goes here</h1>
+            <DayPicker
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+            />
+            <p>You have selected date: {format(selectedDate, "PP")}</p>
           </div>
         </div>
       </div>
